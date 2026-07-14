21776a2ff1f11abbb45036f55586ae5eab4da91b
fabbyfab <128900727+fabbyfab@users.noreply.github.com>
2026-07-14 11:36:10 +0200

    Create questions.ts

---
+++ app/questions.ts
@@ -0,0 +1,28 @@
+export const QUESTIONS_DB = [
+  {
+    question: "Quel fruit épineux et odorant appelle-t-on 'Zaboka' en créole ?",
+    options: ["L'avocat", "Le corossol", "La goyave", "Le fruit à pain"],
+    answer: "L'avocat"
+  },
+  {
+    question: "Quelle île de la Guadeloupe est célèbre pour son tourment d'amour ?",
+    options: ["Marie-Galante", "La Désirade", "Terre-de-Haut", "Grande-Terre"],
+    answer: "Terre-de-Haut"
+  },
+  {
+    question: "En quelle année a eu lieu l'éruption de la Montagne Pelée en Martinique ?",
+    options: ["1898", "1902", "1914", "1928"],
+    answer: "1902"
+  },
+  {
+    question: "Quel est l'ingrédient principal du féroce ?",
+    options: ["Le crabe", "La morue", "Le poulet", "Le lambi"],
+    answer: "La morue"
+  },
+  {
+    question: "Quel célèbre écrivain martiniquais a écrit le 'Cahier d'un retour au pays natal' ?",
+    options: ["Edouard Glissant", "Patrick Chamoiseau", "Aimé Césaire", "Frantz Fanon"],
+    answer: "Aimé Césaire"
+  },
+  // AJOUTE TES AUTRES QUESTIONS ICI... (jusqu'à 200 !)
+];
